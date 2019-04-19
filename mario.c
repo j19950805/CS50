b259fd4da0959fab7761f21a6d46a3fa7ae531b3
j19950805 <j19950805@users.noreply.github.com>
2019-04-19 23:58:53 +0000

    20190419T235145Z

--- mario.c
+++ mario.c
@@ -5,19 +5,25 @@ int main(void)
 {
     int x;
     // Ensure x is between 1 & 8
-    do {
+    do
+    {
         x = get_int("Height:");
-    } while(x > 8 || x <= 0);
+    }
+    while (x > 8 || x <= 0);
     // Print pyramid
-    for(int i=0; i < x; i++) {
-        for(int j=x ; j > i+1; j--) {
+    for (int i = 0; i < x; i++) 
+    {
+        for (int j = x ; j > i + 1; j--) 
+        {
             printf(" ");    
         }
-        for(int k=0; k < i+1; k++) {
+        for (int k = 0; k < i + 1; k++) 
+        {
             printf("#");    
         }
         printf("  ");
-        for(int k=0; k < i+1; k++) {
+        for (int j = 0; j < i + 1; j++) 
+        {
             printf("#");    
         }
         printf("\n");    
