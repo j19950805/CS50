e0c6fef18d89b68956cca1668b0d25a10f4ca84d
j19950805 <j19950805@users.noreply.github.com>
2019-04-19 06:43:52 +0000

    20190419T064330Z

--- mario.c
+++ mario.c
@@ -4,25 +4,20 @@
 int main(void)
 {
     int x,i,j,k;
-    do{
-    x = get_int("Height:");
-    }
-    while(x>8 || x<=0);
-    for(i=0; i<x; i++)
-    {
-        for(k=x ; k>i+1; k--)
-        {
-        printf(" ");    
+    do {
+        x = get_int("Height:");
+    } while(x>8 || x<=0);
+    for(i=0; i<x; i++) {
+        for(k=x ; k>i+1; k--) {
+            printf(" ");    
         }
-        for(j=0; j<i+1; j++)
-        {
-        printf("#");    
+        for(j=0; j<i+1; j++) {
+            printf("#");    
         }
         printf("  ");
-        for(j=0; j<i+1; j++)
-        {
-        printf("#");    
+        for(j=0; j<i+1; j++) {
+            printf("#");    
         }
-    printf("\n");    
+        printf("\n");    
     }
 }
