dcbeb3cbbbeda800c3f930edb1a5706ec7f0ed2d
j19950805 <j19950805@users.noreply.github.com>
2019-04-19 22:15:03 +0000

    20190419T221449Z

--- credit.c
+++ credit.c
@@ -6,27 +6,23 @@ int main(void){
     long x = get_long("Number:");
     int y = x / pow(10,12);
     int digit1, digit2;
-    int sum_digit1 = 0;
-    int sum_digit2 = 0;
+    int sum_digit = 0;
     while(x >= 1)
     {
         digit1 = x / 10 % 10;
+        digit2 = x % 10;
         if (digit1*2 / 10 == 0)
         {
-            sum_digit1 = sum_digit1 + digit1 * 2;   
+            sum_digit = sum_digit + digit1 * 2 + digit2;   
         }
         else
         {
-            sum_digit1 = sum_digit1 + digit1 * 2 % 10 + 1;
+            sum_digit = sum_digit + digit1 * 2 % 10 + 1 + digit2;
         }
-        digit2 = x % 10;
-        sum_digit2 = sum_digit2 + digit2;
         x = x / 100;
     }
-    int result = sum_digit1 + sum_digit2;
     
-    
-    if(result % 10 != 0){
+    if(sum_digit % 10 != 0){
         printf("INVALID\n");
     }
     else if(y / 10 == 34 || y / 10 == 37)
