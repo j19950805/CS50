46548bb17ec5c870fb485f8988b3b0d2adc590cc
j19950805 <j19950805@users.noreply.github.com>
2019-04-19 22:09:10 +0000

    20190419T220858Z

--- credit.c
+++ credit.c
@@ -4,16 +4,16 @@
 
 int main(void){
     long x = get_long("Number:");
-    int y = x/pow(10,12);
+    int y = x / pow(10,12);
     int digit1, digit2;
     int sum_digit1 = 0;
     int sum_digit2 = 0;
     while(x > 1)
     {
         digit1 = x / 10 % 10;
-        if (digit1*2/10 == 0)
+        if (digit1*2 / 10 == 0)
         {
-            sum_digit1 = sum_digit1 + digit1*2;   
+            sum_digit1 = sum_digit1 + digit1 * 2;   
         }
         else
         {
@@ -26,18 +26,18 @@ int main(void){
     int result = sum_digit1 + sum_digit2;
     
     
-    if(result%10 != 0){
+    if(result % 10 != 0){
         printf("INVALID\n");
     }
-    else if(y/10 == 34 || y/10 == 37)
+    else if(y / 10 == 34 || y / 10 == 37)
     {
         printf("AMEX\n");
     }
-    else if(y/100 < 99 && y/100 > 9)
+    else if(y / 100 < 99 && y / 100 > 9)
     {
         printf("MASTERCARD\n");
     }
-    else if(y/1000 == 4 || y == 4)
+    else if(y / 1000 == 4 || y == 4)
     {
         printf("VISA\n");
     }
