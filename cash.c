c848e197f58d52e7aaa7f0ace7ee96bbe03d78b7
j19950805 <j19950805@users.noreply.github.com>
2019-04-19 06:51:19 +0000

    20190419T065050Z

--- cash.c
+++ cash.c
@@ -3,12 +3,7 @@
 #include <math.h>
 
 int main(void){
-    float x;
-    do
-      {
-      x = get_float("Change owed:");
-      }
-    while(x<0);
+    float x = get_float("Change owed:");
     int y = round(x*100);
     int z = y/25 + y%25/10 + y%25%10/5 + y%25%10%5;
     printf("%i\n", z);
