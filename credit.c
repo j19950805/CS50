2ab7182d7d87951afced9f8b5859705c0051a4a5
j19950805 <j19950805@users.noreply.github.com>
2019-04-20 00:09:43 +0000

    20190420T000825Z

--- credit.c
+++ credit.c
@@ -2,15 +2,16 @@
 #include <cs50.h>
 #include <math.h>
 
-int main(void){
+int main(void)
+{
     long x = get_long("Number:");
-    int y = round(x / pow(10,12));
+    int y = round(x / pow(10, 12));
     int sum_digit = 0;
-    while(x >= 1)
+    while (x >= 1)
     {
         int digit1 = x % 10;
         int digit2 = x / 10 % 10;
-        if (digit2*2 / 10 == 0)
+        if (digit2 * 2 / 10 == 0)
         {
             sum_digit = sum_digit + digit2 * 2 + digit1;   
         }
@@ -21,15 +22,19 @@ int main(void){
         x = x / 100;
     }
     
-    if (sum_digit % 10 == 0 && (y / 10 == 34 || y / 10 == 37))
+    if (sum_digit % 10 == 0)
+    {
+        printf("INVALID\n");
+    }
+    else if (y / 10 == 34 || y / 10 == 37)
     {
         printf("AMEX\n");
     }
-    else if (sum_digit % 10 == 0 && (y / 100 < 99 && y / 100 > 9))
+    else if (y / 100 < 99 && y / 100 > 9)
     {
         printf("MASTERCARD\n");
     }
-    else if (sum_digit % 10 == 0 && (y / 1000 == 4 || y == 4))
+    else if (y / 1000 == 4 || y == 4)
     {
         printf("VISA\n");
     }
