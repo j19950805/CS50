67e50948e020a68737dab63310dea1076428d8d9
j19950805 <j19950805@users.noreply.github.com>
2019-04-23 16:16:48 +0000

    20190423T161629Z

---
+++ caesar.c
@@ -0,0 +1,50 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    int key = atoi(argv[1]);
+    if (argc == 2 && key > 0)
+    {
+        string pt = get_string("plaintext: ");
+        printf("ciphertext: ");
+        int i = 0;
+        while (pt[i] != 0)
+        {
+            if (islower(pt[i]))
+            {
+                if (pt[i] + key > 'z')
+                {
+                    printf("%c", pt[i] + key - 26);
+                }
+                else
+                {
+                    printf("%c", pt[i] + key);    
+                }      
+            }
+            else if (isupper(pt[i]))
+            {
+                if (pt[i] + key > 'Z')
+                {
+                    printf("%c", pt[i] + key - 26);
+                }
+                else
+                {
+                    printf("%c", pt[i] + key);    
+                }
+            }
+            else
+            {
+                printf("%c", pt[i]);
+            }
+            i++;
+        }    
+    }
+    else
+    {
+        printf("Usage: ./caesar key");    
+    }
+    printf("\n");
+}
