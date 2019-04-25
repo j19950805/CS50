d15a763ea09df41adfcb9575ad867f433178faec
j19950805 <j19950805@users.noreply.github.com>
2019-04-25 04:11:41 +0000

    20190425T041127Z

--- crack.c
+++ crack.c
@@ -5,16 +5,35 @@
 
 int main(int argc, string argv[])
 {
-    if (argc == 2 && strlen(argv[1]) == 13)
+    if (argc != 2 && strlen(argv[1]) != 13)
     {
-        //get salt value.
-        char a[2];
-        strncpy(a, argv[1], 2);
-        const char *salt = a;
+        printf("Usage: ./crack hash\n");
+        return 1;
+    }
+    //get salt value
+    char a[2];
+    strncpy(a, argv[1], 2);
+    const char *salt = a;
         
-        char key[6] = "";
-        //test one-charaters
-        for (key[0] = 'A'; key[0]  <= 'z'; key[0]++)
+    char key[6] = "";
+    //test one-charater
+    for (key[0] = 'A'; key[0]  <= 'z'; key[0]++)
+    {
+        const char *z = crypt(key, salt);
+        if (strcmp(z, argv[1]) == 0)
+        {
+            printf("%s\n", key);
+            return 0;
+        }
+        if (key[0] == 'Z')
+        {
+            key[0] = 'a' - 1;
+        }   
+    }
+    //test two-charaters
+    for (key[0] = 'A'; key[0]  <= 'z'; key[0]++)
+    {
+        for (key[1] = 'A'; key[1]  <= 'z'; key[1]++)
         {
             const char *z = crypt(key, salt);
             if (strcmp(z, argv[1]) == 0)
@@ -22,68 +41,90 @@ int main(int argc, string argv[])
                 printf("%s\n", key);
                 return 0;
             }
-            if (key[0] == 'Z')
+            if (key[1] == 'Z')
             {
-                key[0] = 'a' - 1;
-            }   
+                key[1] = 'a' - 1;
+            } 
         }
-        //test two-charaters
-        for (key[0] = 'A'; key[0]  <= 'z'; key[0]++)
+        if (key[0] == 'Z')
         {
-            for (key[1] = 'A'; key[1]  <= 'z'; key[1]++)
-            {
+            key[0] = 'a' - 1;
+        }   
+    }
+    //test three-charaters
+    for (key[0] = 'A'; key[0]  <= 'z'; key[0]++)
+    {
+        for (key[1] = 'A'; key[1]  <= 'z'; key[1]++)
+        {
+            for (key[2] = 'A'; key[2]  <= 'z'; key[2]++)
+            {    
                 const char *z = crypt(key, salt);
                 if (strcmp(z, argv[1]) == 0)
                 {
                     printf("%s\n", key);
                     return 0;
                 }
-                if (key[1] == 'Z')
+                if (key[2] == 'Z')
                 {
-                    key[1] = 'a' - 1;
-                } 
+                    key[2] = 'a' - 1;
+                }
             }
-            if (key[0] == 'Z')
+            if (key[1] == 'Z')
             {
-                key[0] = 'a' - 1;
-            }   
+                key[1] = 'a' - 1;
+            } 
         }
-        //test three-charaters
-        for (key[0] = 'A'; key[0]  <= 'z'; key[0]++)
+        if (key[0] == 'Z')
         {
-            for (key[1] = 'A'; key[1]  <= 'z'; key[1]++)
-            {
-                for (key[2] = 'A'; key[2]  <= 'z'; key[2]++)
-                {    
+            key[0] = 'a' - 1;
+        }   
+    }
+    //test four-charaters
+    for (key[0] = 'A'; key[0]  <= 'z'; key[0]++)
+    { 
+        for (key[1] = 'A'; key[1]  <= 'z'; key[1]++)
+        {
+            for (key[2] = 'A'; key[2]  <= 'z'; key[2]++)
+            {    
+                for (key[3] = 'A'; key[3]  <= 'z'; key[3]++)
+                {
                     const char *z = crypt(key, salt);
                     if (strcmp(z, argv[1]) == 0)
                     {
                         printf("%s\n", key);
                         return 0;
                     }
-                    if (key[2] == 'Z')
+                    if (key[3] == 'Z')
                     {
-                        key[2] = 'a' - 1;
+                        key[3] = 'a' - 1;
                     }
                 }
-                if (key[1] == 'Z')
+                if (key[2] == 'Z')
                 {
-                    key[1] = 'a' - 1;
-                } 
+                    key[2] = 'a' - 1;
+                }
             }
-            if (key[0] == 'Z')
+            if (key[1] == 'Z')
             {
-                key[0] = 'a' - 1;
-            }   
+                key[1] = 'a' - 1;
+            } 
         }
-        //test four-charaters
-        for (key[0] = 'A'; key[0]  <= 'z'; key[0]++)
-        { 
-            for (key[1] = 'A'; key[1]  <= 'z'; key[1]++)
-            {
-                for (key[2] = 'A'; key[2]  <= 'z'; key[2]++)
-                {    
-                    for (key[3] = 'A'; key[3]  <= 'z'; key[3]++)
+        if (key[0] == 'Z')
+        {
+            key[0] = 'a' - 1;
+        }   
+    }
+    //test five-charaters
+    for (key[0] = 'A'; key[0]  <= 'z'; key[0]++)
+    {
+        printf("Processing: %s\n", key);
+        for (key[1] = 'A'; key[1]  <= 'z'; key[1]++)
+        {
+            for (key[2] = 'A'; key[2]  <= 'z'; key[2]++)
+            {    
+                for (key[3] = 'A'; key[3]  <= 'z'; key[3]++)
+                {
+                    for (key[4] = 'A'; key[4]  <= 'z'; key[4]++)
                     {
                         const char *z = crypt(key, salt);
                         if (strcmp(z, argv[1]) == 0)
@@ -91,78 +132,34 @@ int main(int argc, string argv[])
                             printf("%s\n", key);
                             return 0;
                         }
-                        if (key[3] == 'Z')
+                        if (key[4] == 'Z')
                         {
-                            key[3] = 'a' - 1;
+                            key[4] = 'a' - 1;
+                        }
+                        if (strcmp(key, "zzzzz") == 0)
+                        {
+                            printf("Usage: ./crack hash\n");
+                            return 1;
                         }
                     }
-                    if (key[2] == 'Z')
+                    if (key[3] == 'Z')
                     {
-                        key[2] = 'a' - 1;
+                        key[3] = 'a' - 1;
                     }
                 }
-                if (key[1] == 'Z')
+                if (key[2] == 'Z')
                 {
-                    key[1] = 'a' - 1;
-                } 
+                    key[2] = 'a' - 1;
+                }
             }
-            if (key[0] == 'Z')
+            if (key[1] == 'Z')
             {
-                key[0] = 'a' - 1;
-            }   
+                key[1] = 'a' - 1;
+            } 
         }
-        //test five-charaters
-        for (key[0] = 'A'; key[0]  <= 'z'; key[0]++)
+        if (key[0] == 'Z')
         {
-            printf("Processing: %s\n", key);
-            for (key[1] = 'A'; key[1]  <= 'z'; key[1]++)
-            {
-                for (key[2] = 'A'; key[2]  <= 'z'; key[2]++)
-                {    
-                    for (key[3] = 'A'; key[3]  <= 'z'; key[3]++)
-                    {
-                        for (key[4] = 'A'; key[4]  <= 'z'; key[4]++)
-                        {
-                            const char *z = crypt(key, salt);
-                            if (strcmp(z, argv[1]) == 0)
-                            {
-                                printf("%s\n", key);
-                                return 0;
-                            }
-                            if (key[4] == 'Z')
-                            {
-                                key[4] = 'a' - 1;
-                            }
-                            if (strcmp(key, "zzzzz") == 0)
-                            {
-                                printf("Usage: ./crack hash\n");
-                                return 1;
-                            }
-                        }
-                        if (key[3] == 'Z')
-                        {
-                            key[3] = 'a' - 1;
-                        }
-                    }
-                    if (key[2] == 'Z')
-                    {
-                        key[2] = 'a' - 1;
-                    }
-                }
-                if (key[1] == 'Z')
-                {
-                    key[1] = 'a' - 1;
-                } 
-            }
-            if (key[0] == 'Z')
-            {
-                key[0] = 'a' - 1;
-            }   
-        }
-    }
-    else
-    {
-        printf("Usage: ./crack hash\n");
-        return 1;
+            key[0] = 'a' - 1;
+        }   
     }
 }
