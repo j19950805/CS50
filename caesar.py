942f3a1c2e3d6f11b310bca73fb68056d6d051c3
j19950805 <j19950805@users.noreply.github.com>
2019-05-08 21:58:59 +0000

    20190508T215840Z

--- caesar.py
+++ caesar.py
@@ -1,7 +1,7 @@
 from cs50 import get_string
 from sys import argv, exit
 
-if len(argv) != 2 or argv[1].isdigit() == False:
+if len(argv) != 2 or not argv[1].isdigit():
     exit("Usage: ./caesar key")
 
 key = int(argv[1]) % 26
@@ -9,9 +9,9 @@ pt = get_string("plaintext:  ")
 print("ciphertext: ", end="")
 
 for i in pt:
-    if (i.islower() == True):
+    if i.islower():
         print(chr(ord('a') + (ord(i) - ord('a') + key) % 26), end="")
-    elif (i.isupper() == True):
+    elif i.isupper():
         print(chr(ord('A') + (ord(i) - ord('A') + key) % 26), end="")
     else:
         print(i, end="")
